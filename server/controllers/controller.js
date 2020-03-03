257711dd8cc2109c85d8ef2a649a89d7a81948fe
Preston Jenkins <prestonjenkins@Prestons-MacBook-Pro.local>
2020-03-02 23:05:08 -0700

    just a little more. Now im done for the night. Promise.

---
+++ server/controllers/controller.js
@@ -0,0 +1,11 @@
+// const chalk = require("chalk")
+
+// module.exports = {
+//   login: async (req, res) => {
+//     console.log(chalk.red("hit login", req.body))
+//     const db = req.app.get("db")
+//     const { username, password } = req.body
+
+//   },
+// };
+
