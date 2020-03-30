22aa106c4a914ee572b37353d8363e5db5445fa2
Preston Jenkins <prestonjenkins@Prestons-MacBook-Pro.local>
2020-03-30 14:34:25 -0700

    debug

--- server/index.js
+++ server/index.js
@@ -25,10 +25,6 @@ app.use(
 
 app.use(express.static(`${__dirname}/../build`))
 
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../build/index.html'))
-})
-
 massive({
     connectionString: CONNECTION_STRING,
     ssl: {
